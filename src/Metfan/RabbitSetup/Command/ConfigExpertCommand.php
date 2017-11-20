29e8209d44c376b301da30d262649620d162e9fe
metfan <l_ulrich2002@yahoo.fr>
2017-11-20 13:03:10 +0100

    fix option shortcut for Host

--- src/Metfan/RabbitSetup/Command/ConfigExpertCommand.php
+++ src/Metfan/RabbitSetup/Command/ConfigExpertCommand.php
@@ -37,7 +37,7 @@ class ConfigExpertCommand extends Command
             ->setName('rsetup:config:expert')
             ->setDescription('Apply configuration file to RabbitMQ')
             ->addArgument('configFile', InputArgument::REQUIRED, 'Configuration file')
-            ->addOption('host', 'h', InputOption::VALUE_REQUIRED, 'Override host config', null)
+            ->addOption('host', null, InputOption::VALUE_REQUIRED, 'Override host config', null)
             ->addOption('port', null, InputOption::VALUE_REQUIRED, 'Override port config', null)
             ->addOption('user', 'u', InputOption::VALUE_REQUIRED, 'Override user config', null)
             ->addOption('password', 'p', InputOption::VALUE_REQUIRED, 'Override password config', null);
